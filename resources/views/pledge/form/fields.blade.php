ae9aefc1268c21428c04dae773fee4caeb3e3a88
tanjed <tanjed.15.6849@gmail.com>
2021-05-02 19:29:21 +0600

    Form refixed

--- resources/views/pledge/form/fields.blade.php
+++ resources/views/pledge/form/fields.blade.php
@@ -1,101 +1,105 @@
-@if ($errors->any())
-    <div class="alert">
-        <ul>
-            <li class="text-danger">{{ $errors->first() }}</li>
-        </ul>
-    </div>
-@endif
-
 <div class="row">
-    <div class="col-md-12">
-        <form action="{{route('pledges.store')}}" style="margin-left:92px; font-size: 20px" method="POST">
-            {{csrf_field()}}
-            <div class="input-group mb-1">
-                <div class="input-group-prepend">
-                    <span class="input-group-text text-uppercase">Name</span>
-                </div>
-                <input type="text" class="form-control" name="name">
+    <div class="col-md-10">
+        @if ($errors->any())
+            <div class="alert">
+                <ul>
+                    <li class="text-danger">{{ $errors->first() }}</li>
+                </ul>
             </div>
+        @endif
 
-            <div class="input-group mb-1">
-                <div class="input-group-prepend">
-                    <span class="input-group-text text-uppercase">Address</span>
-                </div>
-                <input type="text" class="form-control" name="address">
-            </div>
+        <div class="row">
+            <div class="col-md-12">
+                <form action="{{route('pledges.store')}}" style="margin-left:92px; font-size: 20px" method="POST">
+                    {{csrf_field()}}
+                    <div class="input-group mb-1">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text text-uppercase">Name</span>
+                        </div>
+                        <input type="text" class="form-control" name="name">
+                    </div>
 
-            <div class="input-group mb-1">
-                <div class="input-group-prepend">
-                    <span class="input-group-text text-uppercase">city / state / zip</span>
-                </div>
-                <input type="text" class="form-control" name="city_state">
-            </div>
+                    <div class="input-group mb-1">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text text-uppercase">Address</span>
+                        </div>
+                        <input type="text" class="form-control" name="address">
+                    </div>
 
-            <div class="input-group mb-1">
-                <div class="input-group-prepend">
-                    <span class="input-group-text text-uppercase">Phone</span>
-                </div>
-                <input type="text" class="form-control" name="phone">
-                <div class="input-group-prepend">
-                    <span class="input-group-text text-uppercase">Email</span>
-                </div>
-                <input type="text" class="form-control" name="email">
-            </div>
+                    <div class="input-group mb-1">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text text-uppercase">city / state / zip</span>
+                        </div>
+                        <input type="text" class="form-control" name="city_state">
+                    </div>
 
-            <div class="input-group mb-1">
-                <div class="input-group-prepend">
-                    <span class="input-group-text text-uppercase">total pledge $</span>
-                </div>
-                <input type="text" class="form-control" name="total_pledge">
-            </div>
+                    <div class="input-group mb-1">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text text-uppercase">Phone</span>
+                        </div>
+                        <input type="text" class="form-control" name="phone">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text text-uppercase">Email</span>
+                        </div>
+                        <input type="text" class="form-control" name="email">
+                    </div>
 
-            <div class="input-group mb-1">
-                <div class="input-group-prepend">
-                    <span class="input-group-text text-uppercase">$</span>
-                </div>
-                <input type="text" class="form-control" name="weekly"> Weekly
-                <div class="input-group-prepend">
-                    <span class="input-group-text text-uppercase">$</span>
-                </div>
-                <input type="text" class="form-control" name="monthly"> Monthly
-                <div class="input-group-prepend">
-                    <span class="input-group-text text-uppercase">$</span>
-                </div>
-                <input type="text" class="form-control" name="other"> Other
-            </div>
+                    <div class="input-group mb-1">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text text-uppercase">total pledge $</span>
+                        </div>
+                        <input type="text" class="form-control" name="total_pledge">
+                    </div>
 
-            <div class="input-group mb-1">
-                <div class="input-group-prepend">
+                    <div class="input-group mb-1">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text text-uppercase">$</span>
+                        </div>
+                        <input type="text" class="form-control" name="weekly"> Weekly
+                        <div class="input-group-prepend">
+                            <span class="input-group-text text-uppercase">$</span>
+                        </div>
+                        <input type="text" class="form-control" name="monthly"> Monthly
+                        <div class="input-group-prepend">
+                            <span class="input-group-text text-uppercase">$</span>
+                        </div>
+                        <input type="text" class="form-control" name="other"> Other
+                    </div>
+
+                    <div class="input-group mb-1">
+                        <div class="input-group-prepend">
                     <span class="input-group-text">
                         I/We wish to give to "Forward in Faith" by means of a gift other then cash.
                     </span>
-                </div>
-            </div>
-            <div class="input-group mb-1">
-                <div class="input-group-prepend">
-                    <span class="input-group-text text-uppercase">Description</span>
-                </div>
-                <input type="text" class="form-control" name="description">
-            </div>
-            <div class="input-group mb-1">
-                <div class="input-group-prepend">
-                    <span class="input-group-text text-uppercase">Total $</span>
-                </div>
-                <input type="text" class="form-control" name="total">
-            </div>
-            <br>
-            <div class="input-group mb-1">
-                <button class="btn"
-                        type="submit"
-                        style="background-color: #e86638;color: white;width: 30%">SUBMIT
-                </button>
-            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-1">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text text-uppercase">Description</span>
+                        </div>
+                        <input type="text" class="form-control" name="description">
+                    </div>
+                    <div class="input-group mb-1">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text text-uppercase">Total $</span>
+                        </div>
+                        <input type="text" class="form-control" name="total">
+                    </div>
+                    <br>
+                    <div class="input-group mb-1">
+                        <button class="btn"
+                                type="submit"
+                                style="background-color: #e86638;color: white;width: 30%">SUBMIT
+                        </button>
+                    </div>
 
-            <div class="input-group mb-1">
-            <small class=" mt-4">1500 W Cowing Dr. Munic, IN 47304 <br>
-                Phone: 765-744-6292 . RisingHopeChurch.com <br>
-                mhewitt@RisingHopeChurch.com</small>
+                    <div class="input-group mb-1">
+                        <small class=" mt-4">1500 W Cowing Dr. Munic, IN 47304 <br>
+                            Phone: 765-744-6292 . RisingHopeChurch.com <br>
+                            mhewitt@RisingHopeChurch.com</small>
+                    </div>
+                </form>
             </div>
-        </form>
+        </div>
     </div>
 </div>
